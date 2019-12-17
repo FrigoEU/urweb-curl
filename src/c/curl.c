f600c093998a259db6aec476118c13b0f55b980b
Simon Van Casteren <simon.van.casteren@gmail.com>
2019-12-17 09:32:02 +0100

    Segfault: try 1

--- src/c/curl.c
+++ src/c/curl.c
@@ -102,18 +102,18 @@ static uw_Basis_string nonget(const char *verb, uw_context ctx, uw_Basis_string 
     if (lastVerb && (verb ? !strcmp(lastVerb, verb) : !lastVerb[0])) {
       uw_Basis_string lastBody = uw_get_global(ctx, "curl.lastBody");
       if (lastBody && (body ? !strcmp(lastBody, body) : !lastBody[0])) {
-        uw_Basis_string lastAuth = uw_get_global(ctx, "curl.lastAuth");
-        if (lastAuth && (auth ? !strcmp(lastAuth, auth) : !lastAuth[0])) {
-          uw_Basis_string lastUserPwd = uw_get_global(ctx, "curl.lastUserPwd");
-          if (lastUserPwd && (userpwd ? !strcmp(lastUserPwd, userpwd) : !lastUserPwd[0])) {
+        /* uw_Basis_string lastAuth = uw_get_global(ctx, "curl.lastAuth"); */
+        /* if (lastAuth && (auth ? !strcmp(lastAuth, auth) : !lastAuth[0])) { */
+        /*   uw_Basis_string lastUserPwd = uw_get_global(ctx, "curl.lastUserPwd"); */
+        /*   if (lastUserPwd && (userpwd ? !strcmp(lastUserPwd, userpwd) : !lastUserPwd[0])) { */
             uw_Basis_string lastResponse = uw_get_global(ctx, "curl.lastResponse");
             uw_Basis_string lastHttpCodeStr = uw_get_global(ctx, "curl.lastHttpCodeStr");
             if (!lastResponse || !lastHttpCodeStr)
               uw_error(ctx, FATAL, "Missing response in Curl cache");
             return uw_Basis_strcat(ctx, uw_Basis_strcat(ctx, lastHttpCodeStr, ";") , lastResponse);
           }
-        }
-      }
+      /*   } */
+      /* } */
     }
   }
 
